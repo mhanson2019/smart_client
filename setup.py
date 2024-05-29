acd578bec35b56813c9630606fcb26d63fd2a88d
mhanson2019 <hanson7512@gmail.com>
2024-05-28 22:57:20 -0700

    Initial codebase

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smart_client',
+    version='0.1',
+    description='SmartSheet API client wrapper',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author = 'Michael Hanson',
+    author_email = 'michael.hanson@atelier-therapeutics.com',
+    url = 'https://github.com/mhanson2019/smart_client',
+    packages=find_packages(),
+    install_requires=[
+        'smartsheet-python-sdk',
+        'git+https://github.com/mhanson2019/KeyManagement.git#egg=KeyManagement'
+    ],
+    dependency_links=[
+        'https://github.com/mhanson2019/KeyManagement/tarball/master#egg=KeyManagement'
+    ],
+    python_requires='>=3.6',
+    
+)
---
+++ smart_client/dataClasses.py
@@ -0,0 +1,96 @@
+from KeyManagement.getKeys import main as getKeys
+import smartsheet as sm
+
+class SmShBase:
+    def __init__(self, key = None):
+        self.__api_key = getKeys(key)['SmartSheet']
+        self.__client = sm.Smartsheet(self.__api_key)
+
+class DataHandler(SmShBase):
+    def __init__(self,key = None):
+        super().__init__(key)
+        self.client = self._SmShBase__client
+        self._sheetID: str
+        self._colFilter: dict
+        self.sheetRaw: sm.models.Sheet
+        self.sheetFiltered: list
+        self._colMap: dict
+        self._colMapInv: dict
+        
+    @property
+    def sheetID(self):
+        return self._sheetID
+    @sheetID.setter
+    def sheetID(self, value):
+        self._sheetID = value
+    
+    @property
+    def colFilter(self):
+        return self._colFilter
+    @colFilter.setter
+    def colFilter(self, value):
+        self._colFilter = value
+    
+    def get_sheet(self):
+        """Get a sheet by ID"""
+        self.sheetRaw = self.client.Sheets.get_sheet(self._sheetID)
+        self._colMap = {x.title: x.id for x in self.sheetRaw.columns}
+        self._colMapInv = {v: k for k, v in self._colMap.items()}
+        if self._colFilter:
+            rowIDselect = []
+            for i, (key, value) in enumerate(self._colFilter.items()):
+                colID = self._colMap[key]
+                
+                rows = [x for x in self.sheetRaw.rows if x.get_column(colID).value in value]
+                rows = [x.id for x in rows]
+                
+                if i == 0:
+                    rowIDselect.extend(rows)
+                else:
+                    # find intersection of new filter and previous filter
+                    rowsN = set(rows)
+                    rowsO = set(rowIDselect)
+                    rowIDselect = list(rowsN.intersection(rowsO))
+            
+            self.sheetFiltered = [x for x in self.sheetRaw.rows if x.id in rowIDselect]
+        else:
+            self.sheetFiltered = self.sheetRaw.rows
+        
+        return self.sheetFiltered
+
+    def update_rows(self, updateDict: dict):
+        """
+        Update specified cells from select rows in the sheet
+         - updateDict: {rowID: {colID: value}}
+        """
+        rowUpdateList = []
+        
+        for r, value in updateDict.items():
+            update_row = sm.models.Row()
+            update_row.id = r
+            
+            for col, val in value.items():
+                update_cell = sm.models.Cell()
+                update_cell.column_id = col
+                
+                update_cell.value = val['value']
+                if 'link' in val.keys():
+                    update_cell.hyperlink = {'url': val['link']}
+                
+                update_row.cells.append(update_cell)    
+                    
+            rowUpdateList.append(update_row)
+        
+        self.client.Sheets.update_rows(self._sheetID, rowUpdateList)
+
+    
+if __name__ == "__main__":
+    key = "fNI4UrkI8fcDUb16AxzqX-FDL3wNpJcx56eKfXMm2Zw="
+    actions = DataHandler(key)
+    actions.sheetID = "6827322187403140"
+    actions.colFilter = {'Registered': None}
+    sheet = actions.get_sheet()
+    pass
+        
+        
+        
